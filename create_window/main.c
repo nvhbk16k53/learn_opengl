54a83218bdf7dffdd9f5eb7b7aeeec896250283a
HiepNV <nvhbk16k53@gmail.com>
2016-04-05 14:46:06 +0700

    Multi-platform

--- create_window/main.c
+++ create_window/main.c
@@ -17,7 +17,6 @@ int main(int argc, char **argv)
 	glfwInit();
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
-	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 	glfwWindowHint(GLFW_RESIZABLE, GL_FALSE);
 
 	GLFWwindow *window = glfwCreateWindow(800, 600, "LearnOpenGL", NULL, NULL);
